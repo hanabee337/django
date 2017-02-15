464d480ffac2cb3c5d182f34f8ae564867fed292
hanabee337 <hanabee337@gmail.com>
2017-02-15 14:46:04 +0900

    Instagram 만들기 실습
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- instagram/django_app/post/views.py
+++ instagram/django_app/post/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+"""
+Post List를 보여주는 화면을 구성
+1. View에 post_list 함수 작성
+2. Template에 post_list.hrml 파일 작성
+3. view에서 post_list.html을 render한 결과를 리턴하도록 함
+4. instagram/urls.py에 post/urls.py를 연결시킴(app_naem을 post)
+5. '/post/'로 접속했을 때, post_list view에 연결되도록 post/urls.py에 내용을 작성
+6. 전체 post를 가져오는 쿼리셋을 context로 넘기도록 post_list 뷰에 구현
+7. post_list.html에서 {% for %}태그를 사용해 post_list의 내용을 순회하며 표현
+"""
+def post_list(request):
+    """
+
+    :param request:
+    :return:
+    """
