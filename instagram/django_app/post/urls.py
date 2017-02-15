a02f08b946676862d1a19d03bbc3281a1dd5fc00
hanabee337 <hanabee337@gmail.com>
2017-02-15 18:48:37 +0900

    post detail 댓글작성기능 추가
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- instagram/django_app/post/urls.py
+++ instagram/django_app/post/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'post'
 urlpatterns = [
-    url(r'^post$/', views.post_list, name='post_list'),
+    url(r'^post/$', views.post_list, name='post_list'),
 
-    url(r'^post/(?P<post_id>[0-9]+)/', views.post_detail, name='post_detail')
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^post/(?P<post_id>[0-9]+)/comment/add/$', views.comment_add, name='comment_add'),
 ]
--- instagram/django_app/post/views.py
+++ instagram/django_app/post/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Post
+from .models import Post, Comment
 
 """
 Post List를 보여주는 화면을 구성
@@ -22,6 +22,7 @@ def post_list(request):
     }
     return render(request, 'post/post_list.html', context)
 
+
 """
 Post detail(하나의 Post에 대한 상세화면)
 1. View애  post_detail 함수 작성
@@ -30,12 +31,37 @@ Post detail(하나의 Post에 대한 상세화면)
     연결되도록 post/urls.py에 내용 작성
 6. url인자로 전달받은 post_id에 해당하는 Post 객체를
     context에 넘겨 post_detail화면을 구성
-"""
-def post_detail(request, post_id):
 
+post Detail 댓글작성기능 추가
+1. request.method 에 따라 로직이 분리되도록 if/else 추가
+2. request.method = POST 일 경우, request.POST에서 'content' 키의 값을 가져옴
+3. 현재 로그인한 유저는 request.uuser로 가져오묘, Post의 id 값은 post_idㅇ;ㄴ지러 잔딜더되므로 두 내용을 사용
+4. 위 내용물과 content를 사용해서 Comment 객체를 생성 및 저장
+"""
+
+
+def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {
         'post_detail': post
     }
     return render(request, 'post/post_detail.html', context)
 
+
+def comment_add(request, post_id):
+    print('comment_add')
+
+    if request.method == 'POST':
+        print('POST')
+        content = request.POST['content']
+        user = request.user
+        post = Post.objects.get(id=post_id)
+
+        comment = Comment.objects.create(
+            author=user,
+            post=post,
+            content=content
+        )
+        return redirect('post:post_detail', post_id=post_id)
+    else:
+        print('NO POST')
