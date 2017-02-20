29b6b917a1e3c37d5994fb103bd9dc05485747b2
hanabee337 <hanabee337@gmail.com>
2017-02-21 03:47:57 +0900

    ManyToMany 보충 설명 추가
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- documentation/models/djang_app/pizza/models.py
+++ documentation/models/djang_app/pizza/models.py
@@ -10,6 +10,7 @@ class Topping(models.Model):
 
 class Pizza(models.Model):
     title = models.CharField(max_length=100)
+    # Pizza와 Topping은 서로 ManyToMany 관계
     # ManyToMany 관계에서는 아무나 역참조가 되지만,
     # 포함관계에서 상위요소에 해당하는 클래스에 ManyToMany를 선언한다.
     toppings = models.ManyToManyField(Topping)
@@ -19,5 +20,11 @@ class Pizza(models.Model):
             self.title,
             ','.join(self.toppings.values_list('title', flat=True))
         )
+    # flat = True로 하면, tutple이지만, 인자가 하나밖에 없는 경우,
+    # flat=True로 해서, list로 뽑아올 수 있다.
+    # If you only pass in a single field, you can also pass in the flat parameter.
+    # If True, this will mean the returned results are single values,
+    # rather than one-tuples.
+
 
 
