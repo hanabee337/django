b3ec390a8fb421cf25195ccdb3027e2d6679654a
hanabee337 <hanabee337@gmail.com>
2017-02-23 17:15:50 +0900

    facebook : second commit
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- facebook/django_app/ft/tests.py
+++ facebook/django_app/ft/tests.py
@@ -3,3 +3,15 @@ member app을 만들고
 abstractuser를 상속 받는 MyUser르르 구현
 이후 Migration
 """
+
+"""
+로그인 부 생성
+view 생성
+def login_fbv
+
+template 생성
+member/login.html
+
+url연결
+urls.py에 member/login으로 연결되도록
+"""
--- facebook/django_app/member/views.py
+++ facebook/django_app/member/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_fbv(request):
+    return render(request, 'member/login.html')
