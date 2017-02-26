7b9ada556fea5497e9cf96c5c04ea4d93599bec0
hanabee337 <hanabee337@gmail.com>
2017-02-26 16:47:48 +0900

    CustomUser Admin에 등록하기 : 문제 발생하여 다음 commit에서 해결함.
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- youtube/django_app/member/admin.py
+++ youtube/django_app/member/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from member.models import MyUser
+
+# 이렇게만 하면 admin이 잘 동작하지 않음
+# admin에서 다 설정을 해줘야 함.
+# 즉, django customuser를 admin에 등록을 해줄 필요가 있음
+# django customuser admin으로 goolging
+admin.site.register(MyUser)
