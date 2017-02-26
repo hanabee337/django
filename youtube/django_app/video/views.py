d7417b84ee219f7a966f8f825fae2353d5430155
hanabee337 <hanabee337@gmail.com>
2017-02-26 22:33:11 +0900

    SearchToken 구현(2017.02.21) - Next, Prev 버튼 추가 1. Youtube Search API에 요청을 보낸 후, 결과에 	1-2. nextPageToken만 올 경우에는 첫 번째 페이지 	1-3. 둘다 올 경우에는 중간 어딘가 	1-4. prevPageToken만 올 경우에는 마지막 페이지 임을 알 수 있음 2. 템플릿에 nextPageToken, prevPageToken을 전달해서 	해당 token(next또는 prev)값이 있을 경우에 따라 	각각 '다음' 또는 '이전' 버튼을 만들어 줌
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- youtube/django_app/video/views.py
+++ youtube/django_app/video/views.py
@@ -1,10 +1,21 @@
 """
+2017.02.21
 search view의 동작 변경
 1. keyword로 전달받은 검색어를 이용한 결과를 데이터베이스에 저장하는 부분 삭제
 2. 결과를 적절히 가공하거나 그대로 템플릿으로 전달
 3. 템플릿에서는 해당 결과를 데이터베이스를 거치지 않고 바로 출력
 """
-
+"""
+2017.02.21
+Next, Prev버튼 추가
+1. Youtube Search API에 요청을 보낸 후, 결과에
+    1-2. nextPageToken만 올 경우에는 첫 번째 페이지
+    1-3. 둘다 올 경우에는 중간 어딘가
+    1-4. prevPageToken만 올 경우에는 마지막 페이지 임을 알 수 있음
+2. 템플릿에 nextPageToken, prevPageToken을 전달해서
+    해당 token(next또는 prev)값이 있을 경우에 따라
+    각각 '다음' 또는 '이전' 버튼을 만들어 줌
+"""
 import json
 import os
 
@@ -110,8 +121,14 @@ def get_videos(content):
 
 def search(request):
     videos = []
+    context = {
+        'videos': videos,
+    }
 
     print(request.GET)
+
+    page_token = request.GET.get('page_token')
+
     keyword = request.GET.get('keyword', '').strip()
     # request.GET['keyword']로 할 수 도 있지만,
     # 만약, keyword가 없는 경우(keyword에 해당하는 key값이 없는 경우,
@@ -131,21 +148,92 @@ def search(request):
     if keyword != '':
         youtube_id = get_config()
 
+        # 3. requests 라이브러리를 이용(pip install requests), GET요청으로 데이터를 받아온 후
+        # 이렇게 Parameter와 URL을 분리합니다
         params = {
             'part': 'snippet',
             'key': youtube_id,
             'q': keyword,
             'type': 'video',
-            'maxResults': 10
+            'maxResults': 5
         }
+        # 페이지 토큰값이 전달되었을 때만 params에 해당 내용을 추가해서 요청
+        if page_token:
+            params['pageToken'] = page_token
+
         r_dict = requests.get('https://www.googleapis.com/youtube/v3/search', params=params)
         # print(r_dict)
         # print(r_dict.text)
+        # 4. 해당 내용을 다시 json.loads()를 이용해 파이썬 객체로 변환
         content = json.loads(r_dict.text)
         # print(content)
-        videos = get_videos(content)
 
-    context = {
-        'videos': videos,
-    }
+        # 검색결과에서 이전/다음 토큰, 전체 결과 개수를 가져와
+        # 템플릿에 전달할 context객체에 할당한다.
+        # 그런데, .get으로 한 이유는 만약 찾는 게 없으면 None으로 반환되니..
+        # template에서 if 문으로 처리 할 수가 있다.
+        next_page_token = content.get('nextPageToken')
+        print('next_page_token: %s' % next_page_token)
+        prev_page_token = content.get('prevPageToken')
+        print('prev_page_token: %s' % prev_page_token)
+        total_results = content['pageInfo'].get('totalResults')
+        context['next_page_token'] = next_page_token
+        context['prev_page_token'] = prev_page_token
+        context['total_results'] = total_results
+        context['keyword'] = keyword
+
+        # videos = get_videos(content)
+
+        items = content['items']
+        for item in items:
+            # 실제로 사용될 데이터
+            title = item['snippet']['title']
+            published_date_str = item['snippet']['publishedAt']
+            description = item['snippet']['description']
+            youtube_id = item['id']['videoId']
+            thumbnail_url = item['snippet']['thumbnails']['high']['url']
+
+            # pip install python-dateutil
+            published_date = parse(published_date_str)
+
+            # 현재 item을 dict으로 정의
+            video = {
+                'title': title,
+                'description': description,
+                'youtube_id': youtube_id,
+                'publishedAt': published_date,
+                'thumbnail_url': thumbnail_url,
+            }
+            videos.append(video)
+
+            # 단순히 Video.objects.create을 하면 검색할 때 마다
+            # DB에 중복되게 저장이 된다. 이를 방지하기 위해
+            # youtube_id를 unique하게 설정을 하였음.
+            # 그래서, create 대신,
+            # get_or_create(있으면 가져오거나, 없으면 새로 만들어 save함)를 사용하였고,
+            # 그러나, title이나 description이 같은 것들은 걸러내지 못함.
+            # 그래서, get_or_create argumnet에 defaults를 추가함.
+            # get을 했는데, 있으면 object를 가져오고,
+            # 없으면, new object를 create하는데, 이때 defaults 로 설정한
+            # arguments들을 사용함.
+            # get_or_create commit=false로 googling.
+            # defaults = {
+            #     'title': title,
+            #     'description': description,
+            #     'published_date': published_date
+            # }
+            # get_or_created를 안쓰면, try, except로 하는 방법이 있다.
+            # 그러나, 코드가 길어짐.
+            # video, video_created = Video.objects.get_or_create(
+            #     youtube_id=youtube_id,
+            #     defaults=defaults
+            # )
+            # if not video_created:
+            #     print('youtube id: %s ' % youtube_id)
+            #     print('title: %s' % title)
+        # videos = Video.objects.all()
+
+    # context = {
+    #     'videos': videos,
+    # }
     return render(request, 'video/search.html', context)
