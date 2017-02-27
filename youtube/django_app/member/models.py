8a9492b4983a371066528e33bf4f6f04d20c4454
hanabee337 <hanabee337@gmail.com>
2017-02-28 08:07:53 +0900

    YouTube - M2M field에 BookmarkVideo 중간자모델을 사용하면서 발생한 Migration문제 MarkDown 참조
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- youtube/django_app/member/models.py
+++ youtube/django_app/member/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -10,8 +11,21 @@ class Video에서 MTM을 쓰면
 같은 영상에 대해, 같은 정보에 대해 여러 개가 생김. 유저만 다르고.
 그래서, Video에 대해서, User에서 연결을 시켜주는 것이 깔끔해 보임
 """
+
+
 class MyUser(AbstractUser):
     bookmark_videos = models.ManyToManyField(
         'video.Video',
         blank=True,
+        through='BookmarkVideos',
     )
+
+
+# 중간자 모델
+class BookmarkVideos(models.Model):
+    # 중간자 모델이니까 양 쪽 모두에 대해 ForeignKey를 가지고 있어야 되지?
+    # user = models.ForeignKey(MyUser)
+    #  윗줄이나 아랫줄은 같은 의미(settings.py에서 설정)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    video = models.ForeignKey('video.Video')
+    created_date = models.DateTimeField(auto_now_add=True)
