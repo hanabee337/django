9e32ac1a64527803d49a2915bba1cd946d971a6f
hanabee337 <hanabee337@gmail.com>
2017-03-01 16:36:01 +0900

    Django Tutorial - A shortcut: render() : Writing your first Django app, part3
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
@@ -9,12 +10,10 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    templates = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(templates.render(context, request))
-
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
