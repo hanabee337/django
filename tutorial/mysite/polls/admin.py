1789eea599a407aca8825db3b436a34b17b7c532
hanabee337 <hanabee337@gmail.com>
2017-03-02 01:30:05 +0900

    Django Tutorial - Adding related objects : Writing your first Django app, part 7
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- tutorial/mysite/polls/admin.py
+++ tutorial/mysite/polls/admin.py
@@ -3,14 +3,24 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 # admin.site.register(Question)
 # Customize the admin form
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = [
-        (None,              {'fields': ['question_text']}),
-        ('Date Information',{'fields': ['pub_date']}),
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'],
+                              'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+
+
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
+
+# admin.site.register(Choice)
