b04c914e694fc0ab2288168f1b565b1055fc7fec
hanabee337 <hanabee337@gmail.com>
2017-03-02 01:19:24 +0900

    Django Tutorial - Customize the admin form : Writing your first Django app, part 7
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- tutorial/mysite/polls/admin.py
+++ tutorial/mysite/polls/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+# admin.site.register(Question)
+# Customize the admin form
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,              {'fields': ['question_text']}),
+        ('Date Information',{'fields': ['pub_date']}),
+    ]
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
