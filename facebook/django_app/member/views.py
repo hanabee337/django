d7f2b7c092df271066da9bba3dc23da15200dccf
hanabee337 <hanabee337@gmail.com>
2017-03-03 02:21:00 +0900

    Facebook - login 기본 구조 구현
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- facebook/django_app/member/views.py
+++ facebook/django_app/member/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_fbv(request):
+    print('login_fbv')
+    context = {
+
+    }
+    return render(request, 'member/login.html', context)
