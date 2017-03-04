9bbefe447a0ef7e9aad8c9f4e551b08231d5e729
hanabee337 <hanabee337@gmail.com>
2017-03-05 07:42:02 +0900

    Facebook - Template context_processors에 MEDIA_URL표시되도록 media함수 추가
    https://docs.djangoproject.com/en/1.10/ref/settings/의 MEDIA_URL 참조
    
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- facebook/django_app/member/models.py
+++ facebook/django_app/member/models.py
@@ -5,3 +5,9 @@ from django.db import models
 # Create your models here.
 class MyUser(AbstractUser):
     img_profile = models.ImageField(upload_to='user', blank=True)
+
+    def __str__(self):
+        return '{} {}'.format(
+            self.last_name,
+            self.first_name,
+        )
