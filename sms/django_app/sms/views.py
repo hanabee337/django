186337af121515ee0bd2b73bc4226d8f39621371
hanabee337 <hanabee337@gmail.com>
2017-03-05 13:15:02 +0900

    SMS - 전송index뷰 기본 구현(2017.02.24)
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- sms/django_app/sms/views.py
+++ sms/django_app/sms/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from sms.forms import SMSForm
+
 
 def index(request):
-    context = {
+    if request.method == 'POST':
+        print(request.POST)
+        print('recipient_numbers: {}'.format(request.POST['recipient_numbers']))
+        print('content: {}'.format(request.POST['content']))
+        forms = SMSForm(request.POST)
+    else:
+        forms = SMSForm(
+            initial={
+                'recipient_numbers': '010-1234-5678, 01012345678, 01023412, 293849323232, 02394090930'
+            }
+        )
 
+    context = {
+        'forms': forms,
     }
     return render(request, 'common/index.html', context)
