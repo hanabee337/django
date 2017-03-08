aedc4fd0af5b343121c11233ec1589c05e723fd1
hanabee337 <hanabee337@gmail.com>
2017-03-08 14:50:20 +0900

    Deploy EC2 - static_root는 제외(.gitignore)
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- deploy_ec2/django_app/deploy_ec2/settings.py
+++ deploy_ec2/django_app/deploy_ec2/settings.py
@@ -42,7 +42,10 @@ print(config)
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+#static settings
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
@@ -150,7 +153,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-STATIC_URL = '/static/'
