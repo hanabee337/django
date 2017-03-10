64267f2470a37c7f6902ae8a3ab17651ed41ba47
hanabee337 <hanabee337@gmail.com>
2017-03-10 17:32:00 +0900

    Deploy EB(Docker): elastic beanstalk
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- deploy_eb_docker/django_app/config/settings.py
+++ deploy_eb_docker/django_app/config/settings.py
@@ -12,13 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import json
 import os
 
-DEBUG = os.environ.get('MODE') == 'DEBUG'
-STORAGE_S3 = os.environ.get('STORAGE') == 'S3' or DEBUG is False
-DB_RDS = os.environ.get('DB') == 'RDS'
-
-print('DEBUG :{} '.format(DEBUG))
-print('STORAGE_S3 : {}'.format(STORAGE_S3))
-print('DB_RDS : {}'.format(DB_RDS))
+DEBUG = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -50,47 +44,14 @@ for key, key_dict in config_common.items():
         config[key][inner_key] = inner_key_dict
 print(config)
 
-# AWS
-AWS_ACCESS_KEY_ID = config['aws']['access_key_id']
-AWS_SECRET_ACCESS_KEY = config['aws']['secret_access_key']
-
-AWS_S3_HOST = 's3.{}.amazonaws.com'.format(config['aws']['s3_region'])
-AWS_S3_SIGNATURE_VERSION = config['aws']['s3_signature_version']
-AWS_STORAGE_BUCKET_NAME = config['aws']['s3_storage_bucket_name']
-AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
-
 # static settings
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-
-if STORAGE_S3:
-    # Static files
-    STATICFILES_STORAGE = 'deploy_ec2.storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    STATIC_URL = 'https://{custom_domain}/{staticfiles_location}/'.format(
-        custom_domain=AWS_S3_CUSTOM_DOMAIN,
-        staticfiles_location=STATICFILES_LOCATION,
-    )
-    # Media files
-    DEFAULT_FILE_STORAGE = 'deploy_ec2.storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-    MEDIA_URL = 'https://{custom_domain}/{mediafiles_location}/'.format(
-        custom_domain=AWS_S3_CUSTOM_DOMAIN,
-        mediafiles_location=MEDIAFILES_LOCATION
-    )
-else:
-    STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
-    STATIC_URL = '/static/'
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config_common['django']['secret_key']
+SECRET_KEY = config['django']['secret_key']
 
 ALLOWED_HOSTS = config['django']['allowed_hosts']
 
